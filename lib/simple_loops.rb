159f860cbe55de09281a9dfb6bef4bef60499fc7
Felipe Dolago <lagofelipe@gmail.com>
2020-07-26 22:01:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,5 +21,15 @@ def output_array(array)
     puts array[count]
     count += 1
   end
+end
+
+def return_string_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count].to_s
+    count += 1
+  end
+
+  end
 
 end
